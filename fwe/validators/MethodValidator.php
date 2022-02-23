e86f995b1de8342421fa38f9fcd3db509e6cfcce
张宝财 <v_zhangbaocai@saicmotor.com>
2022-02-23 18:02:02 +0800

    fwe\db\Model支持JSON编码接口的实现.

--- fwe/validators/MethodValidator.php
+++ fwe/validators/MethodValidator.php
@@ -18,12 +18,12 @@ class MethodValidator extends IValidator {
 		$attributes = $this->attributes;
 		$validator = $this;
 		$params = compact('validator', 'model', 'isPerOne', 'isOnly', 'attributes');
-        if ($this->method instanceof \Closure) {
+		if ($this->method instanceof \Closure) {
 			return \Fwe::invoke($this->method, $params, null);
-        } elseif(method_exists($model, $this->method)) {
+		} elseif(method_exists($model, $this->method)) {
 			return \Fwe::invoke([$model, $this->method], $params, "{$class}::{$this->method}");
 		} else {
 			trigger_error("{$class}不存在{$this->method}方法", E_USER_ERROR);
-        }
+		}
 	}
 }
